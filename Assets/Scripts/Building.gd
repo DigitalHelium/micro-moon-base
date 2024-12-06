af76ca6fbcbb204cd851f0731d595addc560e0dc
tori-tih <98258927+tori-tih@users.noreply.github.com>
2024-12-07 00:08:31 +0400

    add recource and basic logic

--- Assets/Scripts/Building.gd
+++ Assets/Scripts/Building.gd
@@ -6,3 +6,44 @@ class BuildingClass extends Node2D:
 	
 	enum Type {Base, Module, Observatory, Research, Drill}
 	var tile_positions: Array[BuildingPart]
+	
+	var building_name: String = ""
+	var description: String = ""
+	var cost: BuildingCost
+	
+	signal construction_complites
+	
+	func _init(name: String, desc: String, c: BuildingCost):
+		building_name = name
+		description = desc
+		cost = c
+		
+	# Проверка что можно ставить здание (Нужно будет расширить)
+	func can_construct(res: Dictionary) -> bool:
+		if res.has("metal") and res["metal"].has_amount(cost.metal) and \
+		res.has("energy") and res["energy"].has_amount(cost.energy) and \
+		res.has("science") and res["science"].has_amount(cost.science):
+			return true
+		return false
+	
+	func construct(res: Dictionary) -> bool:
+		if not can_construct(res):
+			return false
+		res["metal"].subtract(cost.metal)
+		res["energy"].subtract(cost.energy)
+		res["science"].subtract(cost.science)
+		
+		construction_complites.emit()
+		return true
+	
+class BuildingCost:
+	var metal: int = 0
+	var energy: int = 0
+	var science: int = 0
+	
+	func _init(m: int = 0, e: int = 0, s: int = 0):
+		metal = m
+		energy = e
+		science = s
+		
+	
---
+++ Assets/Scripts/ResourceManager.gd
@@ -0,0 +1,39 @@
+extends Node
+
+var resource: Dictionary = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	init_resources()
+	pass # Replace with function body.
+	
+func init_resources():
+	resource[GameResource.ResourceClass.Type.Metal] = GameResource.ResourceClass.new(
+		GameResource.ResourceClass.Type.Metal,
+		50,
+		100
+	)
+	resource[GameResource.ResourceClass.Type.Energy] = GameResource.ResourceClass.new(
+		GameResource.ResourceClass.Type.Energy,
+		50,
+		100
+	)
+	resource[GameResource.ResourceClass.Type.Science] = GameResource.ResourceClass.new(
+		GameResource.ResourceClass.Type.Science,
+		50,
+		100
+	)
+	
+	
+func get_resource(type: GameResource.ResourceClass.Type) -> GameResource.ResourceClass:
+	return resource[type]
+	
+func add_resource(type: GameResource.ResourceClass.Type, amount: int) -> bool:
+	return resource[type].add(amount)
+	
+func subtract_resource(type: GameResource.ResourceClass.Type, amount: int) -> bool:
+	return resource[type].subtract(amount)
+	
+func has_resource(type: GameResource.ResourceClass.Type, amount: int) -> bool:
+	return resource[type].has_amount(amount)
+	
