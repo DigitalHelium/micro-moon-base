b4990d933902ace0271885fb51bf19fbab0e8317
tori-tih <98258927+tori-tih@users.noreply.github.com>
2024-12-08 00:37:42 +0400

    menu by tort

--- Assets/Scenes/Camera.gd
+++ Assets/Scenes/Camera.gd
@@ -11,6 +11,10 @@ var min_zoom_l = -10
 var max_zoom_l = 20
 var zoom_speed_l = 0.1
 
+var pos = Vector2(20, 10)
+var pos_b = Vector2(10, 5)
+var k = 1.5
+
 @onready var metal_label = %MetalResource
 @onready var energy_label = %EnergyResource
 @onready var science_label = %ScienceResource
@@ -29,7 +33,10 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			zoom = Vector2.ONE * clamp(zoom.x - zoom_speed, min_zoom, max_zoom)
-			#metal_labe.scale = Vector2(1.5, 1.5)
-			metal_label.position.x -= 30
+			
+			print(get_target_position())
+			print("zoom out")
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			zoom = Vector2.ONE * clamp(zoom.x + zoom_speed, min_zoom, max_zoom)
+			print(get_target_position())
+			print("zoom in")
