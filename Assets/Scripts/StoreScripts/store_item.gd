8d5bd6eee3f7b6c4c53daa2faaa8f18e35f57c33
tori-tih <98258927+tori-tih@users.noreply.github.com>
2024-12-08 04:47:42 +0400

    store button

--- Assets/Scripts/StoreScripts/store_item.gd
+++ Assets/Scripts/StoreScripts/store_item.gd
@@ -5,11 +5,12 @@ var building_manager: BuildingManager;
 
 # Called when the node enters the scene tree for the first time.
 func init(build: Building.BuildingClass, building_manager: BuildingManager):
-	self.text = build.building_name;
+	%name.text = build.building_name;
 	self.build = build;
 	self.building_manager = building_manager;
 	var tile_map = $TileMapLayer
 	building_manager.draw_building_to_map(tile_map, Vector2i(1, 1), build, null, null)
+	
 
 func _ready() -> void:
 	#var tile_map = self.get_child_count()
